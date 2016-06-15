9a3e428fcb0f2811b314c51306a960b68558beda
Alec Goeckel <alecgoeckel@gmail.com>
2016-06-15 14:30:28 -0700

    Add while loop to vampires.rb

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,4 +1,8 @@
+puts "How many employees will be processed?"
+print "> "
+employees = gets.chomp.to_i
 
+while employees > 0
 
 puts "What is your name?"
 print "> "
@@ -16,20 +20,30 @@ puts "Would you like to enroll in the company's health insurance?(y/n)"
 print "> "
 health_insurance = gets.chomp
 
+puts "Please list any allergies you may have."
+  puts "Type \"done\" when finished. "
+    print "> "
+    allergies = gets.chomp
+    until allergies == "done" || allergies == "sunshine"
+      allergies = gets.chomp
+      print "> "
+    end
 
-if age == 2016 - birth_year && garlic_bread == "y" || health_insurance == "y"
-  puts "Probably not a vampire"
-elsif age != 2016 - birth_year && garlic_bread == "y" || health_insurance == "y"
-  puts "Probably a vampire"
-elsif age != 2016 - birth_year && age > 100 &&garlic_bread == "n" && health_insurance == "n"
-  puts "Almost certainly a vampire"
-elsif name == "Drake Cula" || name == "Tu Fang"
-  puts "Definitely a vampire"
-else
-  puts "Results inconclusive"
+
+  if age == 2016 - birth_year && garlic_bread == "y" || health_insurance == "y"
+    puts "Probably not a vampire"
+  elsif age != 2016 - birth_year && garlic_bread == "y" || health_insurance == "y" && allergies == "sunshine"
+    puts "Probably a vampire"
+  elsif age != 2016 - birth_year && age > 100 &&garlic_bread == "n" && health_insurance == "n"
+    puts "Almost certainly a vampire"
+  elsif name == "Drake Cula" || name == "Tu Fang"
+    puts "Definitely a vampire"
+  else
+    puts "Results inconclusive"
+
+  employees -= 1
+  end
 end
-
-
 
 
 
