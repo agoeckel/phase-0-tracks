f80d7ed27bf49cc26cafb64534334affa62e0f0f
Alec Goeckel <alecgoeckel@gmail.com>
2016-06-15 10:07:56 -0700

    Add vampire questionaire to vampires.rb

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+puts "What is your name?"
+print "> "
+name = gets.chomp
+puts "How old are you?"
+print "> "
+age = gets.chomp
+puts "What year were you born?"
+print "> "
+birth_year = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+print "> "
+garlic_bread = gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+print "> "
+health_insurance = gets.chomp
