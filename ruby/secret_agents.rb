56b971951b20c779ead1db8fe8619b2a77ce61b6
Alec Goeckel <alecgoeckel@gmail.com>
2016-06-16 20:35:41 -0700

    Add driver code

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -8,8 +8,14 @@
 # To build a decryption method
 # the output of the encryption method needs to be our input
 # method must break down string to index number
+# Index must be set in a loop to break letters from string
+# Letters will be compared to alphabet and changed to number in
+# relation to alphabet index.
+# Index number will be subtracted by 1.
+# number is assigned to new variable
+# print results
 
-def encryptor(str)
+def encrypt(str)
   index = 0
   while index < str.length
     str[index] = str[index].next!
@@ -18,12 +24,43 @@ def encryptor(str)
   puts str
 end
 
-# def decrypt(str)
-#   index = 0
-#   while index < str.length
-#     str[index] =
+def decrypt(letters)
+index = 0
+result = ""
+  while index < letters.length
+  str = letters[index]
+  result = "abcdefghijklmnopqrstuvqxyz".index(str) - 1
+  result = "abcdefghijklmnopqrstuvqxyz"[result]
+  puts result
+  index += 1
+  end
+end
+
+encrypt("abc")
+encrypt("zed")
+decrypt("bcd")
+decrypt("afe")
 
 
-encryptor("zed     ")
+puts "Would you like to decrypt or encrypt a password?"
+print "> "
+answer = gets.chomp
+
+  if answer == "decrypt"
+    puts "Enter Password"
+    print "> "
+    password = gets.chomp
+    decrypt(password)
+  elsif answer == "encrypt"
+    puts "Enter Password"
+    print "> "
+    password = gets.chomp
+    encrypt(password)
+  else
+    puts "Not Detected"
+    puts "Enter Password"
+    print "> "
+    answer = gets.chomp
+  end
 
 
