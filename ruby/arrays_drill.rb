19f70c7e7559d237d551946579913850ddbd88a6
Alec Goeckel <alecgoeckel@gmail.com>
2016-06-21 18:21:55 -0700

    Add release 2 methods and calls

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,3 +1,14 @@
+def array_output(value1, value2, value3)
+  empty_array = []
+  empty_array += [value1, value2, value3]
+end
+
+
+def array_arg(array=[],argument)
+  array << argument
+end
+
+
 empty_array = []
 p empty_array
 
@@ -20,3 +31,8 @@ another_array = ["stuff1", "stuff2", "stuff3"]
 new_array = empty_array + another_array
 p new_array
 
+p array_output("hi", false, 3)
+p array_arg(["a","b","c",1,2], "a")
+
+p array_output("trial1", 34, "test")
+p array_arg(["testarg", "testing"], 50)
