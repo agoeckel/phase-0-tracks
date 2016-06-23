228ba12bc91d7bdd195b727365efaa3be8bd36c3
Alec Goeckel <alecgoeckel@gmail.com>
2016-06-22 21:16:28 -0700

    Add nested data structures to file as well as method calls

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,43 @@
+race = {
+  'moto1' => [
+    'Ryan Rodgers',
+    'Chris Douglas',
+    'Luke Perry',
+    'Ben Ward',
+    'Jeff Resnick'
+    ],
+  'moto2' => [
+    'Glenn Tarr',
+    'Brendon Connors',
+    'Rick Block',
+    'Greg Jackson',
+    'Flinn Sargon'
+    ],
+  'moto3' => [
+    'Ryan Dungy',
+    'Brett Lodger',
+    'Trent Flether',
+    'Peter Quick'
+  ],
+  'Top 3 winners' => [
+    'First Place' => [],
+    'Second Place' => [],
+    'Third Place' => []
+  ]
+}
+
+race['Top 3 winners'][0]['First Place'] << race['moto1'][2]
+race['Top 3 winners'][0]['First Place'] << race['moto2'][0]
+race['Top 3 winners'][0]['First Place'] << race['moto3'][3]
+race['Top 3 winners'][0]['Second Place'] << race['moto2'][1]
+race['Top 3 winners'][0]['Third Place'] << race['moto3'][0]
+
+race['Top 3 winners'][0]['First Place'].each do |name|
+  name.upcase!
+end
+
+race['moto1'].delete_if {|name| name == 'LUKE PERRY'}
+
+
+
+puts race
