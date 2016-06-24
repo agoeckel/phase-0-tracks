28d4777d172ccf45f4a07bf442ffd967f6b9a42c
Alec Goeckel <alecgoeckel@gmail.com>
2016-06-23 22:49:43 -0700

    Update arrangement of methods using .map! method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,18 +3,78 @@
 # modify the name by adjusting conditional letters one letter forward in the given conditional parameters
 
 def name_changer(name)
-   name.split('').to_s
-   name.chars.each do |letters|
-     if letters =~ /[aeiou]/
-       letters.tr!('aeiou', 'eioua')
-     else letters.next!
-     end
-   end
+  new_name = name.split(' ')
+  new_name[0], new_name[1] = new_name[1], new_name[0]
+  string = new_name.to_s
+  replace = string.chars.map! do |letter|
+    letter.next
+  end
+  p replace.join('')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  # new_name = name.split(' ')
+  # string = new_name.to_s
+
+
+
+
+
+
+
+
+
+  # name.chars.each do |letter|
+  #   name.join('')
+  #   if letter =~ /[aeiou]/
+  #     letter.tr!('aeiou', 'eioua')
+  #   else
+  #     letter.next!
+  #   end
+  #   array = []
+  #   array << letter
+  #   p array
+  # end
+
+
+  #
+  # letter_split = new_name.to_s.split('')
+  # letter_split.each do |letter|
+  #   if letter == 'a'
+  #     letter = 'e'
+  #   else
+  #     letter.next
+  #   end
+  # end
+
+
+
+
+
+  # letter_split.chars.each do
+
+
+
+
    # new_name = new_name.join('')
 
 
    # p new_name[0]
-   # new_name[0], new_name[1] = new_name[1], new_name[0]
+
 
 
    # puts new_name.class
