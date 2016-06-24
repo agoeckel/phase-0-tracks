f78b5aba16db8f000069b2b067017208962f8be2
Alec Goeckel <alecgoeckel@gmail.com>
2016-06-24 15:10:27 -0700

    Restructure entire method flow, simpler method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,41 +3,57 @@
 # modify the name by adjusting conditional letters one letter forward in the given conditional parameters
 
 def name_changer(name)
-  new_name = name.split(' ')
-  new_name[0], new_name[1] = new_name[1], new_name[0]
-
-  first_name = new_name[0]
-   name1 = first_name.chars.map! do |letter|
+  new_name = name.split('')
+  # new_name[0], new_name[1] = new_name[1], new_name[0]
+     name.chars.map! do |letter|
      if letter =~ /[aeiou]/
       letter.tr('aeiou', 'eioua')
      else
       letter.next
      end
   end
-  name1_array = []
-  name1_array << name1.join('')
 
-  last_name = new_name[1]
-   name2 = last_name.chars.map! do |letter|
-     if letter =~ /[aeiou]/
-      letter.tr('aeiou', 'eioua')
-     else
-      letter.next
-     end
-  end
-  name2_array = []
-  name2_array << name2.join('')
 
-  combine_name = name1_array += name2_array
-  combine_name.join(' ')
+
+
+  # first_name = new_name[0]
+  #  name1 = first_name.chars.map! do |letter|
+  #    if letter =~ /[aeiou]/
+  #     letter.tr('aeiou', 'eioua')
+  #    else
+  #     letter.next
+  #    end
+  # end
+  # # name1_array = []
+  # # name1_array <<
+
+  # last_name = new_name[1]
+  #  name2 = last_name.chars.map! do |letter|
+  #    if letter =~ /[aeiou]/
+  #     letter.tr('aeiou', 'eioua')
+  #    else
+  #     letter.next
+  #    end
+  # end
+  # @combine = name2.join('') + name1.join('')
+  # p combine
+  # name2_array = []
+  # name2_array << name2.join('')
+
+  # combine_name = name1_array += name2_array
 end
 
-puts 'Please enter a name'
-name = gets.chomp
-p name_changer(name)
+# index = 3
+# until index <= 0
+#   puts 'Please enter a name'
+#   @name = gets.chomp.downcase
+#   name_changer(@name)
+#   @empty_array = []
+#   @empty_array << @combine
+# end
+# index =- 1
 
+#   p @empty_array
 
-
-
-
+p name_changer('Felicia Torres')
 
