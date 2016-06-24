2cb7f3e47b5c7fc466fdb4cf069601b917e348bc
Alec Goeckel <alecgoeckel@gmail.com>
2016-06-23 20:32:18 -0700

    Reorder methods to manipulate strings output

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,19 +3,38 @@
 # modify the name by adjusting conditional letters one letter forward in the given conditional parameters
 
 def name_changer(name)
-   new_name = name.split(' ')
-   new_name[0], new_name[1] = new_name[1], new_name[0]
-   name = new_name.to_s
+   name.split('').to_s
    name.chars.each do |letters|
      if letters =~ /[aeiou]/
        letters.tr!('aeiou', 'eioua')
-     else
-      letters.next!
+     else letters.next!
      end
-     array = []
-     array.push(letters).join('')
-     puts array
    end
+   # new_name = new_name.join('')
+
+
+   # p new_name[0]
+   # new_name[0], new_name[1] = new_name[1], new_name[0]
+
+
+   # puts new_name.class
+   # new_name = new_name.split('')
+   # p new_name
+
+
+
+
+   #
+
+
+
+       # letters.next!
+   # end
+
+     # array = []
+     # array.push(letters)
+     # puts array[0]
+     # index.times {|letter| p letter + letter}
 
 
     # vowels = each_letter.select! {|vowel| vowel =~ /[aeiou]/}
@@ -27,6 +46,6 @@ empty_array = []
 
 puts "Please type your name"
 print "> "
-name = gets.chomp
-p name_changer(name)
+# name = gets.chomp
+p name_changer('Felicia Torres')
 
