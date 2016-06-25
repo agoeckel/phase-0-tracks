f5d1e447af1f57af67c77667b2450fb4e5ff9f7e
Alec Goeckel <alecgoeckel@gmail.com>
2016-06-24 21:01:54 -0700

    Rearrange loop method call to push values into array

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,6 +2,9 @@
 # Modify the name by initiating specific conditions such as (a,e,i,o,u)
 # modify the name by adjusting conditional letters one letter forward in the given conditional parameters
 
+
+@mod_name = []
+
 def name_changer(name)
   name_split = name.split('')
   @letter_array = []
@@ -17,27 +20,30 @@ def name_changer(name)
   word.gsub!('!', ' ')
   array_word = word.split(' ')
   array_word[0], array_word[1] = array_word[1], array_word[0]
-  @full_name = []
-  @full_name << array_word.join(' ')
-end
 
+  @fullname = array_word.join(' ')
+end
 
 
 puts 'what is your name?'
 name = gets.chomp
+
 @empty_array = []
 until name == 'quit'
-  name_changer(name)
+
   @empty_array << name
+  name_changer(name)
+
+  @mod_name.push(@fullname)
   puts 'give me another name'
   name = gets.chomp
+
 end
 
 
 
-@empty_array.each_index do |index|
-  puts "#{@empty_array[index]} is really #{@full_name[index]}"
-end
+p @empty_array
+p @mod_name
 
 
 
