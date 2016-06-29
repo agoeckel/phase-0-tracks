bac49004403f46d552b1561dcbe984f4528d8f74
Alec Goeckel <alecgoeckel@gmail.com>
2016-06-29 16:32:25 -0700

    Add iteration of santas being initialized

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -14,10 +14,32 @@ class Santa
     speak
     @gender = gender
     @ethnicity = ethnicity
+    @age = 0
+    @reindeer_ranking = ["Rudolph", "Dasher", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
   end
 
 end
 
-realsanta = Santa.new
-realsanta.eat_milk_and_cookies('Chocolate cookie')
-realsanta.speak
+# realsanta = Santa.new
+# realsanta.eat_milk_and_cookies('Chocolate cookie')
+# realsanta.speak
+
+santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+
+
+gender = ['agender', 'female', 'bigender', 'male', 'gender fluid', 'N/A']
+ethnicity = ['white', 'black', 'latino', 'Japanese', 'prefer not to say', 'Mystical Creature (unicorn)', 'N/A']
+
+gender.length.times do |index|
+  santas << Santa.new(gender[index], ethnicity[index])
+end
+
+p santas
+
