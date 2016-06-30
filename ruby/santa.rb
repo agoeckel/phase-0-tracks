11baa19b35c39450136884194ce384a21d426343
Alec Goeckel <alecgoeckel@gmail.com>
2016-06-29 19:01:31 -0700

    Add 1000 santas and iterate through santas at random

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,6 @@
 class Santa
+  attr_reader :ethnicity, :reindeer_ranking
+  attr_accessor :age, :gender
 
   def speak
     p "HO, HO, HO!! Meeerrry Christmas!"
@@ -18,25 +20,25 @@ class Santa
     @reindeer_ranking = ["Rudolph", "Dasher", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
   end
 
-  def age
-    @age
-  end
+  # def age
+  #   @age
+  # end
 
-  def gender
-    @gender
-  end
+  # def gender
+  #   @gender
+  # end
 
-  def ethnicity
-    @ethnicity
-  end
+  # def ethnicity
+  #   @ethnicity
+  # end
 
-  def reindeer_ranking
-    @reindeer_ranking
-  end
+  # def reindeer_ranking
+  #   @reindeer_ranking
+  # end
 
-  def gender=(new_gender)
-    @gender = new_gender
-  end
+  # def gender=(new_gender)
+  #   @gender = new_gender
+  # end
 
   def age=(new_age)
     @age = new_age
@@ -57,7 +59,7 @@ end
 # realsanta.eat_milk_and_cookies('Chocolate cookie')
 # realsanta.speak
 
-santas = []
+# santas = []
 # santas << Santa.new("agender", "black")
 # santas << Santa.new("female", "Latino")
 # santas << Santa.new("bigender", "white")
@@ -70,13 +72,26 @@ santas = []
 gender = ['agender', 'female', 'bigender', 'male', 'gender fluid', 'N/A']
 ethnicity = ['white', 'black', 'latino', 'Japanese', 'prefer not to say', 'Mystical Creature (unicorn)', 'N/A']
 
-gender.length.times do |index|
-  santas << Santa.new(gender[index], ethnicity[index])
+# gender do |index|
+#   santas << Santa.new(gender[index], ethnicity[index])
+# end
+
+# p santas
+
+# p santas[0].age = 50
+# p santas[0].celebrate_birthday
+# p santas[0].get_mad_at('Donner')
+# p @reindeer_ranking
+
+santas = 1000
+random = 100
+
+santas.times do |num|
+   num = Santa.new(gender.sample, ethnicity.sample)
+   num.age = rand(1..140)
+  p num
 end
 
-p santas
 
-p santas[0].age = 50
-p santas[0].celebrate_birthday
-p santas[0].get_mad_at('Donner')
-p @reindeer_ranking
+
+
