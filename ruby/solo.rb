429d7be4a17e4a77eb78117baca5efa5363c95c2
Alec Goeckel <alecgoeckel@gmail.com>
2016-06-30 18:40:19 -0700

    Add until loop and iterate through array of user instances

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -23,8 +23,8 @@
 # output: a trick the rider performed
 
 class Motorcyle_rider
-  attr_reader :bike_make
-  attr_accessor :age, :name, :place, :action
+  attr_reader :place, :action, :mph
+  attr_accessor :age, :name, :bike_make
 
   def initialize(name, age, bike_make)
     puts "Initializing new rider....."
@@ -33,26 +33,51 @@ class Motorcyle_rider
     @bike_make = bike_make.capitalize
   end
 
-  def speed(mph)
-    mph = rand(mph..250)
-    "#{mph} MPH"
+  def speed
+    mph = rand(1..250)
+    "#{@name} is going #{mph} MPH"
   end
 
   def location(place)
-    puts "Is in beautiful #{place}"
+    puts "#{@name} is in beautiful #{place}"
   end
 
   def trick(action)
-    puts "Just #{action}"
+    puts "#{@name} just #{action}!!"
   end
 
 end
 
 
 
-riders = ['MCspeed', 'Pastu', 'Dusty', 'Daren', 'Firsty', 'Tooquick', 'Seeya']
+# p mcspeed = Motorcyle_rider.new('mcspeed', 50, 'honda')
+# p mcspeed.speed
+ # mcspeed.location('San Diego')
+ # mcspeed.trick('Wheelied')
 
-p mcspeed = Motorcyle_rider.new('mcspeed', 50, 'honda')
-p mcspeed.speed(20)
- mcspeed.location('San Diego')
- mcspeed.trick('Wheelied')
+action = ['Wheelied', 'Crashed', 'Burned out', 'Disappeared', 'Jumped', 'got pulled over']
+place = ['San Diego', 'Los Angeles', 'Chicago', 'New York', 'Texas', 'San Francisco']
+
+user_input = []
+
+ puts "Motorcycle Rider creator 1.0 (type 'done' to end)"
+ puts "Continue? (y/n)"
+ start = gets.chomp
+
+   until start == 'done' || start == 'n'
+     puts "Name your rider"
+     @name = gets.chomp.capitalize
+     puts "How old is your rider?"
+     @age = gets.chomp.to_i
+     puts "What kind of motorcycle are they riding?"
+     @bike_make = gets.chomp
+     puts "CONTINUE?"
+     start = gets.chomp
+     user_input << Motorcyle_rider.new(@name, @age, @bike_make)
+   end
+
+user_input.each do |index|
+  p index
+  index.trick(action.sample)
+  index.location(place.sample)
+end
