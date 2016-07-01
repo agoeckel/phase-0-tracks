34978b36a06ff12ed50eeb69656c424b099a7f28
Alec Goeckel <alecgoeckel@gmail.com>
2016-06-30 17:14:03 -0700

    Add pseudocode to solo.rb

---
+++ ruby/solo.rb
@@ -0,0 +1,30 @@
+To build a class named motorcycle_race
+
+initialize method
+input: an integer and an array of types of motorcycles and an array of riders names
+steps: call initialize method with appropriate arguments
+  steps: create attributes (rider, age, motorcycle_make)
+output: the initialized class with corresponding parameters
+
+speed method
+input: integer
+steps: create a speed method that takes an integer as a parameter.
+  steps: default speed method inside the initalize method to 0
+output: an integer
+
+positon method
+input: random numbers
+steps:
+output:
+
+crash method
+input:
+steps:
+output:
+
+
+
+
+
+
+
