27d129065db48f949ce974c4ef47b2b67f094c23
Alec Goeckel <alecgoeckel@gmail.com>
2016-07-13 19:14:47 -0700

    Add refactoring and driver code

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -10,38 +10,54 @@ require_relative 'state_data'
 
 class VirusPredictor
 
+  #initializes our VirusPredictor class with three attributes as parameters
+
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+  #calls method predicted_deaths and speed_of_spread with specific attribute parameters
+
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
+
+  #private method would change the access availability for the methods below the method.
 
   private
 
-  def predicted_deaths(population_density, population, state)
+  #method takes three parameters and filters them through a if else conditional statement.
+  # Outputting the print statement.
+
+  def predicted_deaths
     # predicted deaths is solely based on population density
+
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      num = 0.4
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      num = 0.3
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      num = 0.2
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      num = 0.1
     else
-      number_of_deaths = (@population * 0.05).floor
+      num = 0.05
     end
+
+    number_of_deaths = (@population * num).floor
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+  #method is taking two parameters and filtering them through a if else conditional statement
+  # once the parameter is met with its corresponding conditional, speed is assigned its associated
+  # variable
+
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -70,17 +86,22 @@ end
  # initialize VirusPredictor for each state
 
 
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
+# alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+# alabama.virus_effects
 
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
+# jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
+# jersey.virus_effects
 
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
+# california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
+# california.virus_effects
 
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
+# alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
+# alaska.virus_effects
+
+STATE_DATA.each do |state, information|
+  selected_state = VirusPredictor.new(state, information[:population_density], information[:population])
+  puts selected_state.virus_effects
+end
 
 
 #=======================================================================
